5b69eef1b2753a440bd5c6401a894e2a1b3c09a1
dlzh <2853123+dlzh@users.noreply.github.com>
2024-04-12 10:55:58 +0930

    Created the project and initialized it

---
+++ src/main/java/spring/guru/dzmsscservice/DzMsscServiceApplication.java
@@ -0,0 +1,13 @@
+package spring.guru.dzmsscservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzMsscServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DzMsscServiceApplication.class, args);
+	}
+
+}
